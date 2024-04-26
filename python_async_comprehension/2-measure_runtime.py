93ca3607f4a5d0e613328f1035123291994ff0a5
Felipe Santos <felipecassiors@gmail.com>
2024-04-26 18:38:54 -0400

    2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
-"""Module for learning async programming through practical application."""
+"""
+Module for learning async programming through practical
+application.
+"""
 
 import time
 import asyncio
 
 # Import the async_comprehension coroutine from another module
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__(
+    '1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    Measures the runtime of executing the async_comprehension coroutine
-    four times concurrently.
+    Measures the runtime of executing the async_comprehension
+    coroutine four times concurrently.
 
-    This function utilizes asyncio.gather to run multiple instances of
-    async_comprehension simultaneously and measures the total runtime.
+    This function utilizes asyncio.gather to run multiple
+    instances of async_comprehension simultaneously and
+    measures the total runtime.
 
     Returns:
-        float: The total time taken to execute the coroutine four times,
-               measured in seconds.
+        float: The total time taken to execute the coroutine
+               four times, measured in seconds.
     """
     start = time.time()
-    tasks = [async_comprehension()
-             for _ in range(4)]  # Prepare coroutine instances
-    await asyncio.gather(*tasks)  # Execute all coroutines concurrently
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
     end = time.time()
     return end - start  # Calculate and return the elapsed time
 
