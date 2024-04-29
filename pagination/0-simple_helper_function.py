ac2499d5eb473122bd788fb14a7a2cc0fad78545
LAB <lab@kali>
2024-04-29 11:16:09 -0400

    3-main.c

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 """calculates the start and end indexes"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
-    """Calculate start and end index for pagination."""
-    if page < 1 or page_size < 1:
-        exit(1)
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    """
+    Calculate the start and end index for pagination.
+
+    Args:
+    page (int): The current page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
