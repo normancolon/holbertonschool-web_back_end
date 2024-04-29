c57ec7d6d908c3950295bac491e1c4cabb0bacf1
LAB <lab@kali>
2024-04-29 01:02:44 -0400

    11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -9,6 +9,3 @@ def schools_by_topic(mongo_collection, topic):
     """
     return mongo_collection.find({'topics': topic})
 
-
-if __name__ == '__main__':
-    schools_by_topic(mongo_collection, topic)
