26a88b2b91445ab68bc03ff14b95909f4e866f83
LAB <lab@kali>
2024-05-06 12:15:14 -0400

    12-createReportObject.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,19 +1,12 @@
-// Using const in taskFirst since the variable does not change
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  // Using let in taskNext since the variable 'combination' changes
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  
-  // Assuming the function getLast() is exported and remains unchanged
-  export function getLast() {
-    return ' is okay';
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+export function getLast() {
+  return ' is okay';
+}
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
