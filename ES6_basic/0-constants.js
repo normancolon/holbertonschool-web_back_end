22ca93696f3254d81a70d638725272d31751c9e3
LAB <lab@kali>
2024-05-07 00:10:13 -0400

    11-createEmployeesObject.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -3,11 +3,8 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-export function getLast() {
-  return ' is okay';
-}
+
 export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
+  const combination = 'But sometimes let is okay';
   return combination;
 }
