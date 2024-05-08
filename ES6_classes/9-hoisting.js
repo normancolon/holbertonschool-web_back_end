26a6d2756e39ffea04431fde72cd4c96b2dd7776
LAB <lab@kali>
2024-05-07 21:02:34 -0400

    9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,5 +1,6 @@
-// Define the HolbertonClass first since it needs to be instantiated before StudentHolberton
-class HolbertonClass {
+// Correctly declare classes before their instances are created.
+
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -14,9 +15,8 @@ class HolbertonClass {
   }
 }
 
-// Define the StudentHolberton class
-class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) { // Added the missing parameter
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -30,22 +30,19 @@ class StudentHolberton {
     return this._holbertonClass;
   }
 
-  // Correct use of 'this' instead of 'self'
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Instantiate the HolbertonClass objects
+// Create instances after the class definitions.
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Instantiate the StudentHolberton objects
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Exporting the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
