abb3f9a0aaa0c462a94f236379149c963b1a54dd
LAB <lab@kali>
2024-05-07 20:12:33 -0400

    9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,29 +1,51 @@
-/**
- * Represents an educational cohort at Holberton School.
- */
-export class HolbertonClass {
-    /**
-     * Constructs a HolbertonClass with a year and location.
-     * @param {number} year - The year the class started.
-     * @param {string} location - The geographic location of the class.
-     */
-    constructor(year, location) {
-      if (typeof year !== 'number') {
-        throw new TypeError('Year must be a number');
-      }
-      if (typeof location !== 'string') {
-        throw new TypeError('Location must be a string');
-      }
-      this._year = year;
-      this._location = location;
-    }
-  
-    get year() {
-      return this._year;
-    }
-  
-    get location() {
-      return this._location;
-    }
+// Define the HolbertonClass first since it needs to be instantiated before StudentHolberton
+class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
-  
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+// Define the StudentHolberton class
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) { // Added the missing parameter
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  // Correct use of 'this' instead of 'self'
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+// Instantiate the HolbertonClass objects
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// Instantiate the StudentHolberton objects
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+// Exporting the list of students
+export const listOfStudents = [student1, student2, student3, student4, student5];
