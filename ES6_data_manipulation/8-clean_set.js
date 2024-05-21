8d8c4500df385b3dd5b5079423f8f3fec66bf02e
LAB <lab@kali>
2024-05-21 04:33:52 -0400

    0-get_list_students.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [];
+
+  set.forEach(value => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+}
+
+export default cleanSet;
